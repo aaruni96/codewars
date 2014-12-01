080fa21472b720192ca605542cf8d14a0889ffd1
Aaruni Kaushik <aaruni96@users.noreply.github.com>
2014-12-01 12:16:28 +0530

    Prob02
    The comma input problem!
    
    Took a while to convert string to int array.
    
    
    Signed-off-by: aaruni96 <aaruni.kaushik@gmail.com>

---
+++ prob02.cpp
@@ -0,0 +1,77 @@
+//aah! the comma input! let's see how long it takes to solve this one
+
+//started at 10:55 PM
+
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int sez=1;
+int ar[50];
+
+int convert(char*);
+
+int check(int *);
+
+int main()
+{
+	int n,lel=0;
+	char in[151];
+	
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		if(lel==0)
+		{
+			cin.getline(in,151);
+		}
+		cin.getline(in,151);
+		convert(in);
+		check(ar);
+		lel++;
+	}
+	return 0;
+}
+
+int convert(char *in)
+{
+	int temp=0;
+	int len=strlen(in);
+	int l=0;
+	for(int i=0;i<len;i++)
+	{
+		if(in[i]!=',')
+		{
+			temp+=int(in[i])-'0';
+			temp*=10;
+		}
+		else if((in[i]==',')||(in[i+1]=='\0'))
+		{
+			ar[l]=temp;
+			temp=0;
+			l++;
+			sez++;
+		}
+	}
+	ar[l]=temp;
+	return 0;
+}
+
+int check(int *ar)			//this baby works just fine.
+{
+	int dif=ar[1]-ar[0];
+	int rdif;
+	
+	for(int i=2;i<sez-1;i++)
+	{
+		rdif=ar[i+1]-ar[i];
+		if(dif!=rdif)
+		{
+			cout<<"No\n";
+			return -1;
+		}
+	}
+	cout<<"Yes\n";
+	return 0;
+}
